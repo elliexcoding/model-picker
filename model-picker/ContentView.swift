2b36f52c424355c1c950878d26758daa56a0bdde
Ellie <ladieexstepho@gmail.com>
2021-03-13 20:00:16 +0900

    focus dependenc, note: init out of date

--- model-picker/ContentView.swift
+++ model-picker/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import FocusEntity
 
 struct ContentView : View {
     @State private var isPlacementEnabled = false
@@ -88,9 +89,47 @@ struct ARViewContainer: UIViewRepresentable {
             }
         }
     }
+}
+
+class CustomARView: ARView {
+    let focusSquare = FocusEntity()
+    
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        focusSquare.delegate = self
+        focusSquare.setAutoUpdate(to: true)
+        
+        self.setupARView()
+    }
+    
+    @objc required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupARView() {
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        // lidar tracking
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        self.session.run(config)
+    }
     
 }
 
+extension CustomARView: FocusEntityDelegate {
+    func toTrackingState() {
+        print("Tracking")
+    }
+    
+    func toInitializingState() {
+        print("Initializing")
+    }
+}
 
 // Model Picker view
 struct ModelPickerView: View {
