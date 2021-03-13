f0be57bb6d6773f0f698ea86b345ab4aa99936a1
Ellie <ladieexstepho@gmail.com>
2021-03-13 19:02:15 +0900

    async load model contents for loading

--- model-picker/model.swift
+++ model-picker/model.swift
@@ -14,3 +14,27 @@ import Combine
 
 // async model loading
 
+class Model {
+    var modelName: String
+    var image: UIImage
+    var modelEntity: ModelEntity?
+    
+    private var cancellable: AnyCancellable? = nil
+    
+    init(modelName: String) {
+        self.modelName = modelName
+        
+        self.image = UIImage(named: modelName)!
+        
+        let filename = modelName + ".usdz"
+        self.cancellable = ModelEntity.loadModelAsync(named: filename).sink(receiveCompletion: { loadCompletion in
+            // error handling
+            print("DEBUG: Cannot laod model entity for model name: \(self.modelName)")
+        }, receiveValue: {
+            // fetch model entity
+            modelEntity in
+            self.modelEntity = modelEntity
+            print("DEBUG: Loaded model entity for model name: \(self.modelName)")
+        })
+    }
+}
