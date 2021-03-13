298f9d8d81b30db764731798b96fb95d5d037d08
Ellie <ladieexstepho@gmail.com>
2021-03-13 18:12:30 +0900

    async model file created

---
+++ model-picker/model.swift
@@ -0,0 +1,16 @@
+//
+//  model.swift
+//  model-picker
+//
+//  Created by Eugene Kim on 2021/03/13.
+//
+
+import Foundation
+import UIKit
+import RealityKit
+// async framework
+import Combine
+
+
+// async model loading
+
